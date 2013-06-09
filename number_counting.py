0b794ceb222a785b6fc6cce8900bf44d04b85503
Ethan Soergel <esoergel@gmail.com>
2013-06-09 15:38:48 -0400

    added number_counting

--- number_counting.py
+++ number_counting.py
@@ -1,1 +1,6 @@
-# http://www.dimagi.com/about/careers/exercises/
+def no_sevens(n):
+    total = 0
+    for i in range(1, n+1):
+        if "7" not in str(i):
+            total += 1
+    return total
